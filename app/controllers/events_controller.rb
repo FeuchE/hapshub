b86698568c16df4dc33a8c6815cf7edec4726be7
Ellen Feuchtwanger <feuche@gmail.com>
2025-05-27 17:56:11 +0100

    Events new form partials and rendering in states

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,17 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  # def create
+  #   @event = Event.new(event_params)
+  #   if @event.save
+  #     redirect_to @event, notice: 'Event was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
 end
