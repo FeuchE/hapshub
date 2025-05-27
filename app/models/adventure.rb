7933b5e4d6ea708796264619f601da6d87e7486d
Ellen Feuchtwanger <feuche@gmail.com>
2025-05-27 16:50:10 +0100

    Fixed adventure dependency

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,5 +1,5 @@
 class Adventure < ApplicationRecord
-  belongs_to :event
+  belongs_to :event, optional: true
   has_many :events
   has_many :votes, dependent: :destroy
   has_many :users, through: :votes
