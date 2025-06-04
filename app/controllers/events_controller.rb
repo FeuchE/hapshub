470540ef916d38c85aa004b9926f672318f57606
Nathan Stephenson <nstephenson1887@gmail.com>
2025-06-04 18:03:49 +0100

    calendar added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,19 +2,32 @@ class EventsController < ApplicationController
   before_action :set_group, only: %i[new create show]
   before_action :set_event, only: %i[show destroy]
   before_action :authorize_event_owner!, only: %i[destroy]
+    skip_before_action :authenticate_user!, only: [:index]
 
-  def index
-  if params[:group_id].present?
-    @group = Group.find(params[:group_id])
-    @events = @group.events
-  else
-    @events = Event.all
+# app/controllers/events_controller.rb
+def index
+  @events = Event.all
+
+  respond_to do |format|
+    format.html
+    format.json do
+      render json: @events.map { |event|
+        {
+          id: event.id,
+          start: event.start_time.to_date.to_s,
+          end: (event.end_time.to_date + 1).to_s,
+          title: '', # Empty so no text shows
+          url: Rails.application.routes.url_helpers.event_path(event)
+        }
+      }
+    end
   end
 end
 
 
+
   def show
-    @event = Event.find(params[:id])
+    # @event already set by before_action
   end
 
   def new
@@ -23,19 +36,27 @@ end
 
   def create
     @event = Event.new(event_params)
-    @event.group = @group
+    @event.group = @group if @group.present?
     @event.user = current_user
-    if @event.save
-      redirect_to event_adventures_path(@event), notice: 'Event was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to event_adventures_path(@event), notice: 'Event was successfully created.' }
+        format.json { render json: @event, status: :created }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-def destroy
-  @event.destroy
-  redirect_to events_path, notice: "Event deleted successfully."
-end
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_path, notice: "Event deleted successfully." }
+      format.json { head :no_content }
+    end
+  end
 
   private
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,17 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-
   def home
     if user_signed_in?
       @upcoming_events = current_user.events.where("start_time >= ?", Time.current).order(:start_time)
     else
       @upcoming_events = []
     end
+
+    @event_dates = @upcoming_events.map do |event|
+      {
+        title: event.adventure&.name || event.location || "Event",
+        start: event.start_time.iso8601,
+        end: event.end_time.iso8601
+      }
+    end.to_json
   end
 end
